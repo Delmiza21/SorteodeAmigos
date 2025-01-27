f65847e82bd3ea0dc44d16b3666a78018539b66b
Delmiza21 <delmipongo@gmail.com>
2025-01-27 16:32:00 -0500

    Version1

---
+++ app.js
@@ -0,0 +1,49 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//creando un array que almacenara los nombre de los amigos
+let amigos = [];
+//alert('hola');
+
+function borrarCaja(){
+    let valorCaja=document.querySelector('#amigo');
+    valorCaja.value='';
+}
+
+function agregarAmigo(){
+    //obtenemos valor del input y guardamos en una variable
+    let nombreAmigo= document.getElementById('amigo').value;
+    
+    if (nombreAmigo == ''){
+        alert('Por favor, inserte un nombre');
+    } else {
+        //agregando a la lista de amigos
+        amigos.push(nombreAmigo);
+        
+        //funcion recorre
+        recorreAmigos();
+
+        borrarCaja();
+    }
+}
+
+function recorreAmigos(){
+    let Lista = document.getElementById('listaAmigos');
+    Lista.innerHTML = ""
+    for(let i =0 ; i<amigos.length ; i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        Lista.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+    if(amigos.length > 0){
+        let cantidad = amigos.length;
+        let indice = Math.floor(Math.random()*cantidad+1)
+        console.log(amigos[indice]);
+        let ganador = document.getElementById('resultado'); 
+        ganador.innerHTML = amigos[indice];
+    }else{
+        alert('No hay nombres que sortear');
+    }
+}
